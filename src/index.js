e16e8f0f36ab5a09de6bdb828662cc71565faeaa
benja27 <50673336+benja27@users.noreply.github.com>
2023-03-31 11:53:25 -0600

    Add js functionality

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-
+/*eslint-disable */
 import './style.css';
+
 import Tasks from './modules/tasks.js';
+import { toInteger } from 'lodash';
 
 const tasks = new Tasks();
+let index;
 
-tasks.show();
+document.querySelector('#task-form').addEventListener('submit',(e) =>{
+  e.preventDefault()
+  if(document.querySelector("#newName").value === ""){        
+    tasks.add()
+  }else if(document.querySelector("#newName").value !== ""){        
+    let newName = document.querySelector("#newName").value    
+    tasks.edit(index, newName)
+  }   
+  document.querySelector("#task-input").value = " "
+})
 
-// setInterval(() => {
-//   location.reload()
-// }, 2000);
 
-
+document.querySelector('#list').addEventListener('click',e=>{
+  if(e.target.matches(".edit")){ 
+    let targetIndex = toInteger(e.srcElement.getAttribute("data-index"))        
+    index = targetIndex
+    document.querySelector(".btn-submit").setAttribute("type","button")
+  }
+  if(e.target.matches(".fa-trash")){
+    let index = toInteger(e.srcElement.getAttribute("data-index"))    
+    tasks.delete(index)
+  }
+})
---
+++ src/modules/task.js
@@ -0,0 +1,7 @@
+export default class Task {
+  constructor(name, status, index) {
+    this.name = name;
+    this.status = status;
+    this.index = index;
+  }
+}
